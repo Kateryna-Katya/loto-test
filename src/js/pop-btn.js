da160344ed39c42b7ee7e0b62e13673606f4a2f2
Kateryna <katyaukr2014@gmail.com>
2025-11-18 13:51:29 +0200

    add footer

--- src/js/pop-btn.js
+++ src/js/pop-btn.js
@@ -2,12 +2,10 @@ const openBtns = document.querySelectorAll(".openPopupBtn");
 const popupWrapper = document.getElementById("popupWrapper");
 const closePopup = document.getElementById("closePopup");
 const form = document.getElementById("sendForm");
-const responseMessage = document.querySelector(".response-msg");
 
 openBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     popupWrapper.style.display = "flex";
-    responseMessage.textContent = "";
   });
 });
 
@@ -26,13 +24,6 @@ form.addEventListener("submit", function(e) {
   const url = `https://example.com/submit?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`;
   console.log("GET-запит на:", url);
 
-  responseMessage.textContent = "Успішно відправлено!";
-  responseMessage.style.color = "green";
-
   popupWrapper.style.display = "none";
   form.reset();
-
-  setTimeout(() => {
-    responseMessage.textContent = "";
-  }, 5000);
 });
